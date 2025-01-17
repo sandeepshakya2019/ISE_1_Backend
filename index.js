0f6faef9605c873b7468b447018aec1fc98bd98e
SandeepShakya <sandeepshakya2015@gmail.com>
2025-01-17 17:01:17 +0530

    adding environment variables

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-
-const port = 3002;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Simple API Server");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
