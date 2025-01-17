57131c42a566fd04ea4a53bc5bf4105c0316d9a9
SandeepShakya <sandeepshakya2015@gmail.com>
2025-01-17 17:07:54 +0530

    convert everything into module js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
-const express = require("express");
+import dotenv from "dotenv";
+import express from "express";
+
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Simple API Server");
+  res.send(`Welcome to the Simple API Server ${process.env.APP_NAME}`);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3006, () => {
   console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
