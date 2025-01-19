f9bf51570d7ec61248343344aa03b8a8fd574ab4
SandeepShakya <sandeepshakya2015@gmail.com>
2025-01-19 17:11:50 +0530

    addin mongoose aggregation pagineate and json web token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,23 +1,47 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import jwt form "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
     mobileNo: {
       type: String,
+      unique: true,
       required: [true, "Mobile No is required"],
       validate: {
         validator: function (v) {
           return /^\d{10}$/.test(v);
         },
-        message: (props) => `${props.value} is not a valid mobile number!`,
+        message: (props) => `${props.value} is not a valid mobile number !!`,
       },
+      index: true,
+    },
+    fullName:{
+      type: String,
+      required: [true, "Full Name is required"],
     },
     emailId: {
       type: String,
-      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      unique: true,
+      // match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
       lowercase: true,
       required: false,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid emiail id !!`,
+      },
     },
+    refreshToken:{
+      type: String,
+      required: false,
+    }
+    // userName:{
+    //   type: String,
+    //   unique: true,
+    //   required: [true, "User Name is required"],
+    // }
     // isKYC: {
     //   type: Boolean,
     //   default: false,
@@ -47,5 +71,37 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.plugin(mongooseAggregatePaginate);
+
+// jwt is bearer token 
+userSchema.pre("save", async function (next) {
+  // this.
+  next();
+})
+
+// Adding custom methods for jwt
+userSchema.methods.generateToken = async function () {
+  jwt.sign({
+    _id:this._id,
+    mobileNo: this.mobileNo,
+    fullName: this.fullName
+  }, 
+  process.env.ACCESS_TOKEN_SECRET, 
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  })
+}
+
+userSchema.methods.RefreshToken = async function () {
+  jwt.sign({
+    _id:this._id,
+  }, 
+  process.env.REFRESH_TOKEN_SECRET, 
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  })
+}
+
 
 export const User = mongoose.model("User", userSchema);
