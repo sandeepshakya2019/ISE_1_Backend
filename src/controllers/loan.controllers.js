8057ce80a1adf6e6b51b8157f8ded75b18731065
SandeepShakya <sandeepshakya2015@gmail.com>
2025-01-25 18:07:34 +0530

    fix

--- src/controllers/loan.controllers.js
+++ src/controllers/loan.controllers.js
@@ -74,6 +74,7 @@ const repayLoan = asyncHandler(async (req, res) => {
     user.offeredAmount += Number(loan.totalLoanAmount);
     user.noOfLoan = user.noOfLoan - 1;
     await user.save();
+    await loanId.save();
     return res
       .status(200)
       .json(new ApiResponse(200, loan, "[+] Loan Repaid Successfully"));
