4faab43a469876e23a0e8d41d0f4f9febca7b5f2
SandeepShakya <sandeepshakya2015@gmail.com>
2025-01-29 16:07:45 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(
   express.json({
     limit: "256kb",
